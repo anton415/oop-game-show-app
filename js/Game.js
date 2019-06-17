4f43155145e4824361de0052689364ff99a7526c
anton415 <anton415@gmail.com>
2019-06-17 12:39:31 +0300

    Add basic methods for Game. Add realization of some Phrase methods.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,71 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+ class Game {
+ 	/**
+ 	 * @missed: used to track the number of missed guesses by the player.
+ 	 * @phrases: an array of five Phrase objects to use with the game.
+ 	 * @activePhrase: THis is the Phrase object that's currently in play.
+ 	 */
+ 	constructor(missed = 0, phrases, activePhrase = null) {
+ 		this.missed = missed;
+ 		this.phrases = phrases;
+ 		this.activePhrase = activePhrase;
+ 	}
+ 	
+ 	/**
+ 	 * Hides the start screen overlay, calls the
+ 	 * getRandomPhrase() method, and sets the activePhrase property
+ 	 * with the chosen phrase. It also adds that phrase to the board by
+ 	 * calling the addPhraseToDisplay() method on the active Phrase
+ 	 * object.
+ 	 */
+ 	startGame() {
+ 		
+ 	}
+ 	
+ 	/**
+ 	 * Method randomly retrieves one of the
+ 	 * phrases stored in the phrases array and returns it.
+ 	 */
+ 	getRandomPhrase() {
+ 	
+ 	}
+ 	
+ 	/**
+ 	 * Method controls most of the game
+ 	 * logic. It checks to see if the button clicked by the player matches a
+ 	 * letter in the phrase, and then directs the game based on a correct
+ 	 * or incorrect guess.
+ 	 */
+ 	handleInteraction() {
+ 	
+ 	}
+ 	
+ 	/**
+ 	 * Method removes a life from the scoreboard, by
+ 	 * replacing one of the liveHeart.png images with a lostHeart.png
+ 	 * image (found in the images folder) and increments the missed
+ 	 * property. If the player has five missed guesses,
+ 	 * then end the game by calling the gameOver() method.
+ 	 */
+ 	removeLife() {
+ 	
+ 	}
+ 	
+ 	/**
+ 	 * Method checks to see if the player has
+ 	 * revealed all of the letters in the active phrase.
+ 	 */
+ 	checkForWin() {
+ 	
+ 	}
+ 	
+ 	/**
+ 	 * Method displays the original start screen overlay,
+ 	 * and depending on the outcome of the game, updates the overlay.
+ 	 */
+ 	gameOver() {
+ 	
+ 	}
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,57 @@
-/* Treehouse FSJS Techdegree
+/**
+ * Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * Phrase.js */
+ * Phrase.js 
+ * @author Anton Serdyuchenko
+ * anton415@gmail.com
+ * serdyuchenko.com
+ */
+ class Phrase {
+ 	/**
+ 	 * @param phrase: this is the actual phrase the Phrase object is representing.
+ 	 */
+ 	constructor(phrase) {
+ 		this.phrase = phrase.toLowerCase();
+ 	}
+ 	
+ 	/**
+ 	 * Add letter placeholders to the display when the game starts.
+ 	 */
+ 	addPhraseToDisplay() {
+ 		const ulElement = document.querySelector('ul');
+ 		
+ 		[...this.phrase].forEach(letter => {
+ 			const textNode = document.createTextNode(letter);
+ 			const liElement = document.createElement('li');
+ 			liElement.appendChild(textNode);
+
+ 			if(letter != ' ') {
+ 				liElement.className = 'hide letter ' + letter;
+ 			} else {
+ 				liElement.className = 'space';
+ 			}
+ 			document.querySelector('ul').appendChild(liElement);
+ 		});
+ 	}
+ 	
+ 	/**
+ 	 * Check to see if the letter selected by the player matches a letter in the phrase.
+ 	 */
+ 	checkLetter() {
+ 		
+ 	}
+ 	
+ 	/**
+ 	 * Reveal the letter(s) on the board that matches the player's selection.
+ 	 */
+ 	showMatchedLetter(letter) {
+ 		const letterElements = document.querySelectorAll('.' + letter);
+ 		console.log(letterElements);
+ 		letterElements.forEach(letterElement => {
+ 			letterElement.className = 'show letter ' + letter;
+ 		});
+ 	}
+ }
+const phrase = new Phrase('hello world');
+phrase.addPhraseToDisplay();
+phrase.showMatchedLetter('l')
